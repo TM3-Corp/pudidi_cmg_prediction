4635753135b94c351c50c6e94a212f81e9a266d2
PVSH97 <pvsargent@uc.cl>
2025-08-30 00:38:34 -0400

    Fix Vercel deployment - add cache API and includeFiles

--- api/test.py
+++
@@ -1,9 +0,0 @@
-from http.server import BaseHTTPRequestHandler
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/plain')
-        self.end_headers()
-        self.wfile.write('API Test - Working!'.encode())
-        return
